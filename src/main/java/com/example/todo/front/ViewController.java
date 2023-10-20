89a7dac8e59a9000a3f7cebb983193ee8071722a
RiverCastle <rkdtjd12@gmail.com>
2023-10-20 17:32:04 +0900

    feat: frontend (main page added)

---
+++ src/main/java/com/example/todo/front/ViewController.java
@@ -0,0 +1,22 @@
+package com.example.todo.front;
+
+
+import com.example.todo.api.team.TeamController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/views")
+@RequiredArgsConstructor
+public class ViewController {
+    private final TeamController teamController;
+    @GetMapping("/main")
+    public String main(Model model) {
+        model.addAttribute("teamOverviewDtoList", teamController.searchTeam(""));
+        System.out.println("메인으로 이동");
+        return "main";
+    }
+}
