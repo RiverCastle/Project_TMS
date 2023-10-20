2a1a830d4c6b3e73e861aa56bdb4bc861465b80a
RiverCastle <rkdtjd12@gmail.com>
2023-10-20 17:37:51 +0900

    feat: frontend (login page added)

--- src/main/java/com/example/todo/front/ViewController.java
+++ src/main/java/com/example/todo/front/ViewController.java
@@ -19,4 +19,9 @@ public class ViewController {
         System.out.println("메인으로 이동");
         return "main";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
