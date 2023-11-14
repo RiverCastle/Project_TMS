ec7c5e153cf79a907eaeab72f2b1f5717899382f
RiverCastle <rkdtjd12@gmail.com>
2023-11-14 11:13:49 +0900

    fix: 답글 조회 dto에 id 추가

--- src/main/java/com/example/todo/dto/task/TaskCommentReplyDto.java
+++ src/main/java/com/example/todo/dto/task/TaskCommentReplyDto.java
@@ -11,12 +11,14 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class TaskCommentReplyDto {
+    private Long id;
     private String  writerName;
     @NotNull(message = "내용을 작성해주세요.")
     private String reply;
     private LocalDateTime createdAt;
     public static TaskCommentReplyDto fromEntity(TaskCommentReplyEntity entity) {
         TaskCommentReplyDto replyReadDto = new TaskCommentReplyDto();
+        replyReadDto.setId(entity.getId());
         replyReadDto.setWriterName(entity.getWriter().getUser().getUsername());
         replyReadDto.setReply(entity.getDeletedAt() == null ? entity.getReply() : "삭제된 답글입니다.");
         replyReadDto.setCreatedAt(entity.getCreatedAt());
