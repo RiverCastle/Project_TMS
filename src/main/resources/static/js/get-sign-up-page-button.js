c409a2a2aa133932bdb6240e9b98f8d384d8e9e7
RiverCastle <rkdtjd12@gmail.com>
2023-11-22 01:36:58 +0900

    design: 로그인 페이지 회원가입, 메인 버튼 디자인

--- src/main/resources/static/js/get-sign-up-page-button.js
+++ src/main/resources/static/js/get-sign-up-page-button.js
@@ -1,4 +1,4 @@
-const sign_up_page_button = document.getElementById("to_sing_up_page");
+const sign_up_page_button = document.getElementById("to_sign_up_page");
 sign_up_page_button.addEventListener("click", () => {
     location.href = "/views/sign-up";
 })
